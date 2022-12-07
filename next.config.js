e05a8d19b034af3c9067882cc5292953f0099181
Cisnneros <cisnnerosbcrra@hotmail.com>
2022-12-07 12:09:07 -0600

    allow origin next

--- next.config.js
+++ next.config.js
@@ -7,7 +7,10 @@ const nextConfig = {
     "ACCESS_TOKEN_SECRET": "tKtLY;MAJN79)d+AG^B&sCrL5qj-w4@67YW@z'\qed3@)9Bx",
     "REFRESH_TOKEN_SECRET": "M[{W5e3/X;tdZUR={T'*m)hM&6M^-C~dgsK;WtHcEb9vA`NW4CCJ9xtY~DB<8,~u.5J389w&)PR7ufs",
     "PAYPAL_CLIENT_ID": "AWoSBP1xkiu5S39QlJ7tS3bbTslJX5SvVD1G-7aNikdfJaiAbVEC933udpvPF8wwLV3CEwN4LORBNszr"
-  }
+  },
+  headers: [
+    { key: "Access-Control-Allow-Origin", value: "https://flowers2.vercel.app/api/" },
+  ],
 }
 
 module.exports = nextConfig
